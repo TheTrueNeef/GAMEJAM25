554ca6c3e0787b864a517761e424a01c6641eb8c
Mostafa Elmansory <faxfriend103@hotmail.com>
2025-02-08 18:16:09 -0500

    added timer for spawner

--- EngineeringOverload/Assets/Scripts/BoxSpawner.cs
+++ EngineeringOverload/Assets/Scripts/BoxSpawner.cs
@@ -5,7 +5,15 @@ public class BoxSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject box;
     public float speed = 5f;
+    public float spawnTime = 1;
+    float targetTime;
     void Start()
+    {
+        SpawnBox();
+        targetTime = spawnTime;
+    }
+
+    private void SpawnBox()
     {
         GameObject boxClone = Instantiate(box, transform.position, Quaternion.identity);
         Rigidbody boxRB = boxClone.GetComponent<Rigidbody>();
@@ -17,6 +25,10 @@ public class BoxSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f) {
+            SpawnBox();
+            targetTime = spawnTime;
+        }
     }
 }
